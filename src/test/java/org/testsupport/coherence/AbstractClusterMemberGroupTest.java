2c8b66c61c4366fcc082dd67ac4e79e3dd9553f8
Jonathan Hall <jhall@jhall-t410.(none)>
2011-11-03 21:11:33 +0000

    Ongoing

--- src/test/java/org/testsupport/coherence/AbstractClusterMemberGroupTest.java
+++ src/test/java/org/testsupport/coherence/AbstractClusterMemberGroupTest.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.assertThat;
 public abstract class AbstractClusterMemberGroupTest extends AbstractTest {
     protected static final String TCMP_CLUSTER_MEMBER_CACHE_CONFIG_FILE = "coherence/testsupport-cache-config.xml";
 
-    protected static final int CLUSTER_SIZE_WITHOUT_CLUSTER_MEMBER_GROUP = 1;
     protected static final int SINGLE_TEST_CLUSTER_SIZE = 1;
     protected static final int SMALL_TEST_CLUSTER_SIZE = 2;
     protected static final int MEDIUM_TEST_CLUSTER_SIZE = 3;
