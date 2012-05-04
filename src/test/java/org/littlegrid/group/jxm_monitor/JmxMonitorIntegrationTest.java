74f84af5372bf903f1d919dbd81a7e9c21a9fd25
Jonathan Hall <jon>
2012-05-04 11:37:21 +0100

    #75 - improved JMX test

--- src/test/java/org/littlegrid/group/jxm_monitor/JmxMonitorIntegrationTest.java
+++ src/test/java/org/littlegrid/group/jxm_monitor/JmxMonitorIntegrationTest.java
@@ -5,6 +5,13 @@ import org.junit.Test;
 import org.littlegrid.AbstractAfterTestShutdownIntegrationTest;
 import org.littlegrid.ClusterMemberGroupUtils;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.littlegrid.ClusterMemberGroupTestSupport.CLUSTER_SIZE_WITHOUT_CLUSTER_MEMBER_GROUP;
 import static org.littlegrid.ClusterMemberGroupTestSupport.SINGLE_TEST_CLUSTER_SIZE;
 import static org.littlegrid.ClusterMemberGroupTestSupport.TCMP_CLUSTER_MEMBER_CACHE_CONFIG_FILE;
@@ -15,7 +22,9 @@ import static org.littlegrid.ClusterMemberGroupTestSupport.assertThatClusterIsEx
  */
 public final class JmxMonitorIntegrationTest extends AbstractAfterTestShutdownIntegrationTest {
     @Test
-    public void jmxMonitorClusterMember() {
+    public void jmxMonitorClusterMember()
+            throws Exception {
+
         final int numberOfMembers = SINGLE_TEST_CLUSTER_SIZE;
         final int expectedClusterSize = numberOfMembers + CLUSTER_SIZE_WITHOUT_CLUSTER_MEMBER_GROUP;
 
@@ -25,5 +34,11 @@ public final class JmxMonitorIntegrationTest extends AbstractAfterTestShutdownIn
                 .buildAndConfigureForStorageDisabledClient();
 
         assertThatClusterIsExpectedSize(CacheFactory.ensureCluster(), expectedClusterSize);
+
+        final MBeanServerConnection connection = ManagementFactory.getPlatformMBeanServer();
+        final int jmxClusterSize = (Integer) connection.getAttribute(
+                new ObjectName("Coherence:type=Cluster"), "ClusterSize");
+
+        assertThat(jmxClusterSize, is(expectedClusterSize));
     }
 }
