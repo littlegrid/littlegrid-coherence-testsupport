896f9fca1da420bb8a3e9c4bca5f73aed567b1fb
Jonathan Hall <jon>
2014-03-24 23:31:30 +0000

    Ongoing work on ideas around command socket server

---
+++ src/main/java/org/littlegrid/app/AppUtils.java
@@ -0,0 +1,11 @@
+package org.littlegrid.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhall
+ * Date: 14/03/14
+ * Time: 16:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppUtils {
+}
---
+++ src/main/java/org/littlegrid/app/Shell.java
@@ -0,0 +1,11 @@
+package org.littlegrid.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhall
+ * Date: 17/03/14
+ * Time: 18:15
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Shell {
+}
