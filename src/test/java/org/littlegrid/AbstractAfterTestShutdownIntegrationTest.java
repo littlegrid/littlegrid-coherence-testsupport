ec141388a2d65b500bd7a1bf9d0044707e8fc075
halljon <halljon.github@hall.uk.net>
2020-11-15 22:20:11 +0000

    #4 - tidying after release.

--- src/test/java/org/littlegrid/AbstractAfterTestShutdownIntegrationTest.java
+++ src/test/java/org/littlegrid/AbstractAfterTestShutdownIntegrationTest.java
@@ -32,13 +32,11 @@
 package org.littlegrid;
 
 import org.junit.After;
-import org.junit.Ignore;
 
 /**
  * Abstract base class which provides a cluster member group instance variable and
  * also performs a cluster member group support shutdown after *each* test.
  */
-@Ignore
 public class AbstractAfterTestShutdownIntegrationTest {
     protected ClusterMemberGroup memberGroup;
 
